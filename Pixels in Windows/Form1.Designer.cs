8bfd633e32def1edfabd0714060aa2598b763793
hu5085tt <hu5085tt@T301020-1625303>
2016-10-18 20:09:04 -0500

    GUI design done

--- Pixels in Windows/Form1.Designer.cs
+++ Pixels in Windows/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pixelsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pixelsLabel
+            // 
+            this.pixelsLabel.AutoSize = true;
+            this.pixelsLabel.Location = new System.Drawing.Point(12, 9);
+            this.pixelsLabel.Name = "pixelsLabel";
+            this.pixelsLabel.Size = new System.Drawing.Size(96, 13);
+            this.pixelsLabel.TabIndex = 0;
+            this.pixelsLabel.Text = "Pixels in Window =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pixelsLabel);
+            this.Name = "Form1";
+            this.Text = "Pixel in Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label pixelsLabel;
     }
 }
 
