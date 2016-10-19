8502118b8bf6e069e7b4660c7770929b7a35e82c
hu5085tt <hu5085tt@T301020-1625303>
2016-10-18 20:17:04 -0500

    Updates

--- Pixels in Windows/Form1.Designer.cs
+++ Pixels in Windows/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.Controls.Add(this.pixelsLabel);
             this.Name = "Form1";
             this.Text = "Pixel in Windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
